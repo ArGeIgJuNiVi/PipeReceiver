485a3ecd6e5add31b7789c3169f8350b6936ed77
Nicon-Georgian Albu <nicon@containrr.com>
2024-04-29 11:26:21 +0200

    Pipe receiver with initial length int

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+
+namespace PipeSender;
+
+public class _
+{
+    public static void Main(string[] args)
+    {
+        using AnonymousPipeClientStream pipe = new(PipeDirection.In, args[0]);
+        byte[] lengthBytes = new byte[4];
+        pipe.BeginRead(lengthBytes, 0, 4, null, null);
+        int length = BitConverter.ToInt32(lengthBytes);
+        byte[] bytes = new byte[length];
+        pipe.Read(bytes, 0, length);
+        Console.WriteLine(new string([.. bytes.Select((val) => (char)val)]));
+    }
+}
